251410c02cb8a4e55449cd233e35b4b40fd127da
Nitish Nayan <Nitish Nayan@DESKTOP-OECPI9S>
2024-06-06 16:38:48 +0530

    Base Class And Other update

--- Recorem/src/main/java/objectrepository/CandidateHomePage.java
+++ Recorem/src/main/java/objectrepository/CandidateHomePage.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Reporter;
 
 public class CandidateHomePage
 
@@ -14,6 +15,8 @@ public class CandidateHomePage
 		PageFactory.initElements(driver, this);
 		
 	}
+	
+	
 
 	@FindBy(xpath = "//span[@aria-label='caret-down']")
 	private WebElement profileMenuButton;
