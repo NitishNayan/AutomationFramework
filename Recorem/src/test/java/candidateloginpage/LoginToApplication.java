87f311e3b391be0a466025af540087b1176752c8
Nitish Nayan <Nitish Nayan@DESKTOP-OECPI9S>
2024-06-07 00:08:03 +0530

    Cmt

--- Recorem/src/test/java/candidateloginpage/LoginToApplication.java
+++ Recorem/src/test/java/candidateloginpage/LoginToApplication.java
@@ -12,6 +12,7 @@ public class LoginToApplication extends BaseClass
 	public void TestScript()
 	{
 		Reporter.log("LoginToApplication Test script method",true);
+		Reporter.log("LoginToApplication Test script method",true);
 		hp.getProfileMenuButton().click();	
 		String title = driver.getTitle();
 		Assert.assertEquals(title,"Recorem App | Discover Talent in specialized gatherings", " the title is not correct");
