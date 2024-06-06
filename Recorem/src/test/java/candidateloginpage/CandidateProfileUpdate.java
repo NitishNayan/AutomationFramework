e162032d0fc1a731614960a9b04fd812f624493e
Nitish Nayan <Nitish Nayan@DESKTOP-OECPI9S>
2024-06-06 09:05:59 +0530

    Added code

--- Recorem/src/test/java/candidateloginpage/CandidateProfileUpdate.java
+++ Recorem/src/test/java/candidateloginpage/CandidateProfileUpdate.java
@@ -3,6 +3,7 @@ package candidateloginpage;
 import static org.testng.Assert.fail;
 
 import org.testng.Assert;
+import org.testng.Reporter;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
@@ -14,8 +15,11 @@ public class CandidateProfileUpdate extends BaseClass
 	@Test
 	public void Testscript()
 	{
-		Repoter.log("Changes form git",true);
+		Reporter.log("Changes form git",true);
+		Reporter.log("Code Modified in eclips",true);
 		hp.getProfileMenuButton().click();
+		
+		
 		//Assert.fail();
 		
 		
